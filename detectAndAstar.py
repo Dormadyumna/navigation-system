0aafc5825fde4ee1bc1f0dee9b2bf31a4627f656
Dormadyumna <97785688+Dormadyumna@users.noreply.github.com>
2022-08-20 16:04:53 +0530

    update

--- detectAndAstar.py
+++ detectAndAstar.py
@@ -48,12 +48,14 @@ class Spot:
 		self.centre = (x+w/2, y+h/2)
 
 minContours = []
+total = 0
 
 def getContours(imgCanny, imgCopy):
     contours, _ = cv2.findContours(imgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    i=0
-    for contour in contours:
 
+    i=0
+
+    for contour in contours:
         area = cv2.contourArea(contour)
         if area > 500:
             perimeter = cv2.arcLength(contour,True)
@@ -62,17 +64,27 @@ def getContours(imgCanny, imgCopy):
             x, y, w, h = cv2.boundingRect(approx)
             cv2.rectangle(imgCopy,(x,y),(x+w,y+h),(0,255,0),2)
             cv2.putText(imgCopy, str(i), (x + (w//2),y + (h//2)), cv2.FONT_HERSHEY_PLAIN, 1, (255,0,0), 2, cv2.FILLED)
-            minContours.append(approx)            
+            minContours.append(approx)
             i = i+1
+
+    print("THE TOTAL NO. OF NODES ARE")
+    print(i)
+    global total
+    total = i
 
 
 
 #MAIN
 augDict= loadImages('ArucoDB')
-img = cv2.imread('Images\squareTiles.jpg')
+img = cv2.imread('Images\path.png')
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 imgCanny = cv2.Canny(imgGray,50,50)
 imgCopy = img.copy()
+
+# getContours(imgCanny, imgCopy)
+
+# cv2.imshow('Contour Detection', imgCopy)
+# cv2.waitKey(10000000)
 
 arucoFound = findArucoMarkers(img)
 
@@ -83,51 +95,74 @@ if len(arucoFound[0])!= 0:
 
 getContours(imgCanny, imgCopy)
 
+w_all = []
+h_all = []
+
 id = 0
 spots = []
+
 for cnt in minContours:
     x, y, w, h = cv2.boundingRect(cnt)
+    w_all.append(w)
+    h_all.append(h)
     spots.append(Spot(x,y,w,h,id))
+    
+    print(id)
+    print( (x + w/2, y + h/2) )
+
     id = id + 1
+
+w_sum = 0
+h_sum = 0
+
+for w in w_all:
+    w_sum = w_sum + w
+
+for h in h_all:
+    h_sum = h_sum + h
+
+w_av = w_sum/total
+h_av = h_sum/total
 
 for spot in spots:
     i = 0
     for cnt in minContours:
-        if(cv2.pointPolygonTest(cnt, (spot.centre[0] + w, spot.centre[1]), False)==1):
+        # print(spot)
+        if(cv2.pointPolygonTest(cnt, (spot.centre[0] + w_av, spot.centre[1]), False)==1):
             spot.neighbors.append(i)
-        elif(cv2.pointPolygonTest(cnt, (spot.centre[0] - w, spot.centre[1]), False)==1):
+        elif(cv2.pointPolygonTest(cnt, (spot.centre[0] - w_av, spot.centre[1]), False)==1):
             spot.neighbors.append(i)
-        elif(cv2.pointPolygonTest(cnt, (spot.centre[0] , spot.centre[1] + h), False)==1):
+        elif(cv2.pointPolygonTest(cnt, (spot.centre[0] , spot.centre[1] + h_av), False)==1):
             spot.neighbors.append(i)
-        elif(cv2.pointPolygonTest(cnt, (spot.centre[0] , spot.centre[1] - h), False)==1):
+        elif(cv2.pointPolygonTest(cnt, (spot.centre[0] , spot.centre[1] - h_av), False)==1):
             spot.neighbors.append(i)
         i = i + 1
 
 for spot in spots:
     print(spot.id, spot.neighbors)
 
-start = spots[0]
-end = spots[63]
+begin = 2
+last = 52
+
+start = spots[begin]
+end = spots[last]
 
 hue = []
 for spot in spots:
     hue.append( abs(spot.centre[0]-end.centre[0]) + abs(spot.centre[1]-end.centre[1]) )
-
-print(hue)
-
     
 count = 0
 open_set = PriorityQueue()
-open_set.put( (0, count, 0) ) #f_score, count(for tie breaking if 2 have same f_score), spot_id) 
+open_set.put( (0, count, begin) ) #f_score, count(for tie breaking if 2 have same f_score), spot_id) 
 came_from = {}
-g_score = {spot: float("inf") for spot in range(0,64)}
-g_score[0] = 0
-f_score = {spot: float("inf") for spot in range(0,64)}
-f_score[0] = abs(spots[0].centre[0]-spots[63].centre[0]) + abs(spots[0].centre[1]-spots[63].centre[1])
+g_score = {spot: float("inf") for spot in range(0,i+1)}
+g_score[begin] = 0
+f_score = {spot: float("inf") for spot in range(0,i+1)}
+f_score[begin] = abs(spots[0].centre[0]-spots[last].centre[0]) + abs(spots[0].centre[1]-spots[last].centre[1])
 
 # print(f_score[0])
 
-open_set_hash = {0}
+open_set_hash = {begin}
 
 while not open_set.empty():
     current = open_set.get()[2]
@@ -135,11 +170,11 @@ while not open_set.empty():
     print(current)
     open_set_hash.remove(current)
 
-    if current == 63:
+    if current == last:
         break
     
     for neighbor in spots[current].neighbors:
-        temp_g_score = g_score[current] + 75
+        temp_g_score = g_score[current] + 45
 
         if temp_g_score < g_score[neighbor]:
             came_from[neighbor] = current
@@ -158,6 +193,6 @@ while not open_set.empty():
 # print("\n\n")
 # print(f_score)
 
-
-cv2.imshow('Contour Detection', imgCopy)
+imgFinal = cv2.resize(imgCopy, (1600,800))
+cv2.imshow('Contour Detection', imgFinal)
 cv2.waitKey(10000000)
